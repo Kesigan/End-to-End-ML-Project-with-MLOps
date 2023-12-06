5bcd0bf504bf6b0e0e22b1830d470c4ebc2a5a50
kesigan <kesigan@gmail.com>
2023-12-06 13:00:40 +0530

    added model evaluation and MLFlow

--- README.md
+++ README.md
@@ -12,3 +12,69 @@
 7. Update the pipeline 
 8. Update the main.py
 9. Update the app.py
+
+
+# How to run?
+### STEPS:
+
+Clone the repository
+
+```bash
+https://github.com/entbappy/End-to-end-Machine-Learning-Project-with-MLflow
+```
+### STEP 01- Create a conda environment after opening the repository
+
+```bash
+conda create -n mlproj python=3.8 -y
+```
+
+```bash
+conda activate mlproj
+```
+
+
+### STEP 02- install the requirements
+```bash
+pip install -r requirements.txt
+```
+
+
+```bash
+# Finally run the following command
+python app.py
+```
+
+Now,
+```bash
+open up you local host and port
+```
+
+
+
+## MLflow
+
+[Documentation](https://mlflow.org/docs/latest/index.html)
+
+
+##### cmd
+- mlflow ui
+
+### dagshub
+[dagshub](https://dagshub.com/)
+
+MLFLOW_TRACKING_URI=https://dagshub.com/kesigan/End-to-End-ML-Project-with-MLOps.mlflow \
+MLFLOW_TRACKING_USERNAME=kesigan \
+MLFLOW_TRACKING_PASSWORD=00b33ab2c658f09484834de0d352e2db226d81fe \
+python script.py
+
+Run this to export as env variables:
+
+```bash
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/kesigan/End-to-End-ML-Project-with-MLOps.mlflow
+
+export MLFLOW_TRACKING_USERNAME=kesigan 
+
+export MLFLOW_TRACKING_PASSWORD=00b33ab2c658f09484834de0d352e2db226d81fe
+
+```
--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipe
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-# from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+import os
 
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/kesigan/End-to-End-ML-Project-with-MLOps.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="kesigan"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="00b33ab2c658f09484834de0d352e2db226d81fe"
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -40,6 +44,18 @@ STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
